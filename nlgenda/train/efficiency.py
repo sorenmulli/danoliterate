09cae40e867df7a0d7bc81ac3c50ac4d9b3a22f0
sorenmulli <swholm@protonmail.com>
2023-10-03 22:15:42 +0200

    Implement LoRA using PEFT library

---
+++ nlgenda/train/efficiency.py
@@ -0,0 +1,15 @@
+from omegaconf import DictConfig
+from peft import LoraConfig, PeftModelForCausalLM, TaskType, get_peft_model
+from transformers import PreTrainedModel
+
+
+def setup_lora(model: PreTrainedModel, cfg: DictConfig) -> PeftModelForCausalLM:
+    peft_config = LoraConfig(
+        task_type=TaskType.CAUSAL_LM,
+        inference_mode=False,
+        r=cfg.r,
+        lora_alpha=cfg.alpha,
+        lora_dropout=cfg.dropout,
+    )
+    peft_model = get_peft_model(model, peft_config)
+    return peft_model
--- nlgenda/train/lm_training.py
+++ nlgenda/train/lm_training.py
@@ -16,6 +16,7 @@ from nlgenda.datasets.pretraining import get_streaming_data, tokenize_datasets
 from nlgenda.infrastructure.logging import format_config
 from nlgenda.infrastructure.runs import run_dir, run_name
 from nlgenda.modeling.load_model import from_pretrained_hf_hub_no_disk
+from nlgenda.train.efficiency import setup_lora
 
 logger = logging.getLogger(__name__)
 
@@ -70,12 +71,19 @@ def train_lm(cfg: DictConfig):
     logger.info("Loaded model with %.1f M parameters.", model.num_parameters() / 1e6)
     logger.info("Loaded tokenizer with vocabulary size %i.", tokenizer.vocab_size)
 
+    if cfg.train.lora.enabled:
+        model = setup_lora(model, cfg.train.lora)
+        logger.info(
+            "Set up PEFT with LoRa.\n - Trainable parameters:\t%i\n - Total parameters:\t\t%i",
+            *model.get_nb_trainable_parameters()
+        )
+
     logger.info("Setting up streaming datasets %s ...", " + ".join(cfg.train.data.datasets))
     datasets = get_streaming_data(cfg.train.data)
     datasets = tokenize_datasets(datasets, tokenizer, cfg)
     logger.info(
         "Datasets set up! %i were skipped for test, %i are validation, rest train. "
-        "Division after shuffle with seed=%i",
+        "Splits were divided after shuffle with seed=%i",
         cfg.train.data.test_examples,
         cfg.train.data.validation_examples,
         cfg.train.data.seed,
