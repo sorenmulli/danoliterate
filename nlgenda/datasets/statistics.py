cafebf26755672ebacd56b186f3b4d8e90a15128
sorenmulli <swholm@protonmail.com>
2023-10-11 10:03:09 +0200

    Add hyggeswag category count stats

--- nlgenda/datasets/statistics.py
+++ nlgenda/datasets/statistics.py
@@ -1,6 +1,12 @@
+import logging
 import os
 
-from datasets import Dataset
+import matplotlib.pyplot as plt
+import pandas as pd
+from datasets import Dataset, load_dataset
+from tqdm import tqdm
+
+logger = logging.getLogger(__name__)
 
 
 def text_stats(dataset: Dataset, text_cols: list[str]) -> dict[str, int]:
@@ -16,3 +22,26 @@ def text_stats(dataset: Dataset, text_cols: list[str]) -> dict[str, int]:
         "num_characters": sum(with_counts["chars"]),
         "num_words": sum(with_counts["words"]),
     }
+
+
+def hyggeswag_categories(side_effects=True):
+    source_dataset = load_dataset("hellaswag", split="validation")
+    dataset = load_dataset("sorenmulli/hyggeswag", split="train")
+    activities = {(row["source_id"], row["ind"]): row["activity_label"] for row in source_dataset}
+    activity_labels = []
+    for row in tqdm(dataset):
+        key = (row["source_id"], row["ind"])
+        activity_label = activities[key]
+        activity_labels.append(activity_label)
+    df = pd.DataFrame(dataset)
+    df["activity"] = activity_labels
+    if side_effects:
+        num_unique_activities = len(df["activity"].unique())
+        print(f"Number of unique activities: {num_unique_activities}")
+
+        top_activities = df["activity"].value_counts()
+        print(f"Top activities:\n{top_activities.head()}")
+
+        top_activities.head(10).plot(kind="bar")
+        plt.show()
+    return df
