5e94a628d40216c8b995c79d8995f90a60910094
sorenmulli <swholm@protonmail.com>
2023-12-07 01:11:20 +0100

    Run bert offensive on GPU

--- nlgenda/modeling/text_classification.py
+++ nlgenda/modeling/text_classification.py
@@ -6,6 +6,8 @@ class BatchBertOffensive(BertOffensive):
     def __init__(self, batch_size=32):
         super().__init__()
         self.batch_size = batch_size
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model = self.model.to(self.device)
 
     def _get_batch_pred(self, sentences: list[str]) -> torch.Tensor:
         inputs = self.tokenizer.batch_encode_plus(
@@ -15,9 +17,9 @@ class BatchBertOffensive(BertOffensive):
             max_length=self.max_length,
             truncation=True,
             padding=True,
-        )
+        ).to(self.device)
         with torch.inference_mode():
-            pred = self.model(inputs["input_ids"], token_type_ids=inputs["token_type_ids"])[0]
+            pred = self.model(**inputs)[0]
         return pred
 
     def batch_predict_proba(self, sentences: list[str]) -> list[float]:
@@ -25,7 +27,7 @@ class BatchBertOffensive(BertOffensive):
         for i in range(0, len(sentences), self.batch_size):
             batch_sentences = sentences[i : i + self.batch_size]
             preds = self._get_batch_pred(batch_sentences)
-            probas = torch.nn.functional.softmax(preds, dim=1).detach().numpy()
+            probas = torch.nn.functional.softmax(preds, dim=1).detach().cpu().numpy()
             all_probas.extend(probas)
 
         return [prob.tolist() for prob in all_probas]
