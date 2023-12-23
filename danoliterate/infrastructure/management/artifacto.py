bd50a4c3dc2d9a0d4086c832bd06f448c13d2ad0
sorenmulli <swholm@protonmail.com>
2023-12-23 19:20:47 +0100

    Allow to mark old results as debug

--- danoliterate/infrastructure/management/artifacto.py
+++ danoliterate/infrastructure/management/artifacto.py
@@ -1,5 +1,6 @@
 import sys
 from collections import defaultdict
+from datetime import datetime
 from enum import Enum
 from pathlib import Path
 from typing import DefaultDict
@@ -13,7 +14,7 @@ from wandb import Artifact
 from danoliterate.evaluation.artifact_integration import dict_from_artifact, yield_wandb_artifacts
 from danoliterate.evaluation.results import ExecutionResult
 from danoliterate.infrastructure.logging import logger
-from danoliterate.infrastructure.timing import get_now_stamp
+from danoliterate.infrastructure.timing import FORMAT, from_timestamp, get_now_stamp
 
 LOGO = """
 ░█▀█░█▀▄░▀█▀░▀█▀░█▀▀░█▀█░█▀▀░▀█▀░█▀█
@@ -25,7 +26,7 @@ LOGO = """
 class CleanDebugOptions(Enum):
     DISPLAY_DEBUG = 0
     DELETE_ALL = 1
-    CANCEL = 2
+    CANCEL = None
 
 
 def clean_debug(cfg: DictConfig):
@@ -67,9 +68,9 @@ def clean_debug(cfg: DictConfig):
 
 
 class DupeMarkerOptions(Enum):
-    PRINT_PAIRS = 0
+    PRINT = 0
     MARK_ALL = 1
-    CANCEL = 2
+    CANCEL = None
 
 
 def _format_result(result: Artifact) -> str:
@@ -132,7 +133,7 @@ def mark_dupes(cfg: DictConfig):
             status_bar="Choose operation.",
         ).show()
         match DupeMarkerOptions(dupe_index):
-            case DupeMarkerOptions.PRINT_PAIRS:
+            case DupeMarkerOptions.PRINT:
                 logger.info(
                     "Pairs:\n%s",
                     "\n".join(
@@ -156,15 +157,67 @@ def mark_dupes(cfg: DictConfig):
                 return main_menu(cfg)
 
 
+def mark_before(cfg: DictConfig):
+    while True:
+        timestamp = input(
+            "Input date time before which to mark all results as debug.\n" f"({FORMAT}): "
+        ).strip()
+        try:
+            time = from_timestamp(timestamp)
+            assert (
+                datetime(2023, 1, 1) < time < datetime.now()
+            ), "Time should be between now and primo 2023"
+            break
+        except (AssertionError, ValueError) as error:
+            logger.error("Error with input:\n%s\n", str(error))
+
+    logger.info("Fetching all results ...")
+    results = list(yield_wandb_artifacts(cfg.wandb.project, cfg.wandb.entity))
+    logger.info("Got %i results", len(results))
+
+    to_mark_artifacts = [
+        res for res in results if from_timestamp(res.metadata["timestamp"]) <= time
+    ]
+
+    while True:
+        dupe_index = TerminalMenu(
+            ["Display old ones", "Mark all old as duplicates", "Cancel"],
+            title=LOGO + "Handle duplicates.",
+            status_bar="Choose operation.",
+        ).show()
+        match DupeMarkerOptions(dupe_index):
+            case DupeMarkerOptions.PRINT:
+                logger.info(
+                    f"{len(to_mark_artifacts)} artifacts to mark:\n%s",
+                    "\n".join(_format_result(artifact) for artifact in to_mark_artifacts),
+                )
+            case DupeMarkerOptions.MARK_ALL:
+                for artifact in tqdm(to_mark_artifacts, desc="Marking old duplicates as debug"):
+                    artifact.metadata["evaluation_cfg"]["debug"] = True
+                    artifact.save(cfg.wandb.project)
+                logger.info(
+                    "Marked %i duplicates as debug, persisted with W&B.", len(to_mark_artifacts)
+                )
+                return main_menu(cfg)
+            case DupeMarkerOptions.CANCEL:
+                return main_menu(cfg)
+
+
 class MainMenuOptions(Enum):
     MARK_DUPES = 0
-    CLEAN_DEBUG = 1
-    EXIT = 2
+    MARK_BEFORE = 1
+    CLEAN_DEBUG = 2
+    EXIT = None
 
 
 def main_menu(cfg: DictConfig):
     main_index = TerminalMenu(
-        ["Handle duplicate results", "Delete results marked as debug", "Quit tool"],
+        [
+            "Handle duplicate results",
+            "Mark results before date as duplicate",
+            "Delete results marked as debug",
+            "Quit tool",
+        ],
         title=LOGO + "Welcome to the Artifact Database tool.",
         status_bar="Choose operation.",
     ).show()
@@ -172,6 +225,8 @@ def main_menu(cfg: DictConfig):
     match MainMenuOptions(main_index):
         case MainMenuOptions.MARK_DUPES:
             mark_dupes(cfg)
+        case MainMenuOptions.MARK_BEFORE:
+            mark_before(cfg)
         case MainMenuOptions.CLEAN_DEBUG:
             clean_debug(cfg)
         case MainMenuOptions.EXIT:
