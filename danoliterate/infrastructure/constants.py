f7d21c63d0fe46ea4437cda2b03e3b0f31617394
sorenmulli <swholm@protonmail.com>
2024-04-04 13:15:09 +0200

    Remove W&B Artifact integration, use local Result DB

--- danoliterate/infrastructure/constants.py
+++ danoliterate/infrastructure/constants.py
@@ -5,5 +5,5 @@ _THIS_FILE_PATH = Path(__file__).resolve()
 CONFIG_DIR = str((_THIS_FILE_PATH.parent.parent / "configs").resolve())
 
 REPO_PATH = _THIS_FILE_PATH.parent.parent.parent
-EXECUTION_RESULT_ARTIFACT_TYPE = "evaluation_result"
-SCORES_ARTIFACT_TYPE = "scores"
+EXECUTION_RESULT_NAME = "evaluation_result"
+SCORES_NAME = "scores"
