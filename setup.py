e4255b9c812e06b5038fb7f1d71004bec4ecd41a
sorenmulli <swholm@protonmail.com>
2024-04-04 22:48:48 +0200

    Bump version to 0.0.4

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ for version, file in REQS.items():
 
 setup_args = dict(
     name="danoliterate",
-    version="0.0.3",
+    version="0.0.4",
     packages=find_packages(),
     author="Søren Winkel Holm",
     author_email="swholm@protonmail.com",
